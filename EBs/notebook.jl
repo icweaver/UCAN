bf7147b5a176b72850573477dbbe26b1c1127fbc
icweaver <weaveric@gmail.com>
2024-03-15 23:52:03 -0700

    up

--- EBs/notebook.jl
+++ EBs/notebook.jl
@@ -23,13 +23,13 @@ md"""
 
 	```
 	Observing mode: Exoplanets
-	RA: 09h 43m 45.4705
-	Dec: 55h 57m 09.0667 	
+	Ra: 09h 43m 45.47s
+	Dec: +55° 57' 09.07"   
 	Duration: 4 hours
-	Exposure time: 1400 ms
-	Cadence: 1400 ms
-	Recommended Gain: 0	
-	Max Gain: 1.78
+	Exposure time (ms): 1400
+	Cadence (ms): 1400
+	Recommended Gain (dB): 0
+	Max Gain (dB): 1.78
 	```
 """
 
@@ -41,7 +41,7 @@ Red dot to the right of the Big Dipper
 """
 
 # ╔═╡ 98d76407-2a80-4662-915a-7a43cb72a81f
-Resource("https://github-production-user-asset-6210df.s3.amazonaws.com/25312320/312196802-ed69219e-f169-46a5-b1b0-6a903d9a52f1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240312%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240312T182934Z&X-Amz-Expires=300&X-Amz-Signature=fed3044d5324390c777c04ab739286abee025a1b2932acc2bf2fbd746540b410&X-Amz-SignedHeaders=host&actor_id=25312320&key_id=0&repo_id=242439725")
+LocalResource("./data/finder_chart_WUMa.png")
 
 # ╔═╡ a6dc11ce-bfc9-11ee-2e99-9b68bd0f081e
 md"""
