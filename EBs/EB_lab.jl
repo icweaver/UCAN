95742cb4ad922c934d10b0c9b5a2eec8fbda23db
icweaver <weaveric@gmail.com>
2024-04-28 18:26:00 -0400

    up

--- EBs/EB_lab.jl
+++ EBs/EB_lab.jl
@@ -50,9 +50,16 @@ In this visualization, we see how the observed brightness of an eclipsing binary
 md"""
 ## Introduction
 
-W UMa is an eclipsing binary system located in the [Ursa Major](https://en.wikipedia.org/wiki/Ursa_Major) constellation, which can be seen being chased across the sky by the Big Dipper throughout the night:
+[W Ursae Majoris (W UMa)](https://www.aavso.org/vsots_wuma) is an eclipsing binary system located in the [Ursa Major](https://en.wikipedia.org/wiki/Ursa_Major) constellation, and can be seen being chased across the sky by the Big Dipper throughout the night:
 
+$(Resource("https://github.com/icweaver/UCAN/blob/main/EBs/data/constellation_WUMa.png?raw=true"))
 
+*W UMa is marked by the larger, red dot to the right of the Big Dipper*
+"""
+
+# ╔═╡ c1bbb6a2-6996-4fee-a642-a0212b473474
+md"""
+Discoverd in the early 1900s, this system is composed of two main-sequence F-type stars orbiting so closely together that they are expected to be [contact binaries](https://en.wikipedia.org/wiki/Contact_binary) sharing a common envelope. Their proximity to each other also gives this system an astonishingly short orbital period of just over 8 hours. Because of how neatly this fits into an Earth day, eclipse events occur at almost the same time every night, making them the ideal target for regular follow-up study. According to the [AAVSO ephemeris](https://www.aavso.org/sites/default/files/AAVSO_%20EB_Ephemeris_%202024.pdf) for this system, 
 """
 
 # ╔═╡ abb9a9c8-5cac-4af3-b0a0-b7a3608dfe1a
@@ -2333,7 +2340,8 @@ version = "1.4.1+1"
 # ╠═b4ef12ad-d389-4c11-8932-a16e8e0a0254
 # ╟─aa005b55-626e-41e0-8fe1-137bd7dd5599
 # ╟─4266575e-e19f-48e4-8b21-6f296c6d3f33
-# ╠═aaaaa4d6-737b-4e53-a3a4-fcac09789d4e
+# ╟─aaaaa4d6-737b-4e53-a3a4-fcac09789d4e
+# ╠═c1bbb6a2-6996-4fee-a642-a0212b473474
 # ╠═abb9a9c8-5cac-4af3-b0a0-b7a3608dfe1a
 # ╠═1356c02f-9ff2-491f-b55d-666ee76e6fae
 # ╟─06d26240-81b6-401b-8eda-eab3a9a0fb20
