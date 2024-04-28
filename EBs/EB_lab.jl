1f331ce6a5effc81ec7b0c1c50b425acc1f274df
icweaver <weaveric@gmail.com>
2024-04-28 17:40:48 -0400

    up

--- EBs/EB_lab.jl
+++ EBs/EB_lab.jl
@@ -34,15 +34,7 @@ md"""
 
 It turns out that the alien world described in the [3 Body Problem](https://www.netflix.com/tudum/articles/3-body-problem-teaser-release-date) is not too far off from what we see in reality. Star systems can be made up of just one star like in our system, three as in the tv show and book series from which the 3 Body Problem [draws its inspiration](https://en.wikipedia.org/wiki/Alpha_Centauri), or even as many as six different stars as in this [recently discovered system](https://science.nasa.gov/universe/exoplanets/discovery-alert-first-six-star-system-where-all-six-stars-undergo-eclipses/)! While these would make for some quite interesting sunsets, their stability decreases as more bodies are added. This is partly why the most common star systems we happen to see are singular star systems, followed closely behind by binary star systems, which account for [nearly two-thirds of all star systems in the Milky Way](https://pweb.cfa.harvard.edu/news/most-milky-way-stars-are-single).
 
-A sub-class of this binary star case, known as eclipsing binaries, has proved to be an invaluable tool for helping us learn more about [orbital mechanics and stellar evolution](https://www.aavso.org/introduction-why-are-eclipsing-binary-stars-important). In these types of systems, not only do these two star orbit about their common center-of-mass, but they do so along our line of sight.
-
-EB intro here
-
-But why EBs?
-
-<https://www.aavso.org/introduction-why-are-eclipsing-binary-stars-important>
-
-<https://www.aavso.org/general-notes>
+A sub-class of this binary star case, known as eclipsing binaries, has proved to be an invaluable tool for helping us learn more about [orbital mechanics and stellar evolution](https://www.aavso.org/introduction-why-are-eclipsing-binary-stars-important). In these types of systems, not only do these two star orbit about their common center-of-mass, but they do so along our line of sight. In other words, eclisping binaries are star systems where each star passes in front of the other from our vantage point. In this lab, we will capture this dance in real time in a fairly common constellation.
 """
 
 # ╔═╡ aaaaa4d6-737b-4e53-a3a4-fcac09789d4e
