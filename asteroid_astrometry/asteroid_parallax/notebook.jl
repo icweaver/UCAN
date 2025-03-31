68381f74ba64d9dc22eec4ca0407a942a12b9a87
icweaver <weaveric@gmail.com>
2025-03-31 13:26:50 -0700

    up

--- asteroid_astrometry/asteroid_parallax/notebook.jl
+++ asteroid_astrometry/asteroid_parallax/notebook.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.4
+# v0.20.5
 
 using Markdown
 using InteractiveUtils
@@ -7,7 +7,7 @@ using InteractiveUtils
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
     #! format: off
-    quote
+    return quote
         local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
@@ -32,8 +32,6 @@ begin
 	using AstroImages, PlutoPlotly
 	colormap = :cividis
 	zscale = Zscale(contrast=0.15)
-	AstroImages.set_clims!(zscale)
-	AstroImages.set_cmap!(colormap)
 	
 	# Analysis
 	using CoordinateTransformations, ImageTransformations, LinearAlgebra, OrderedCollections
@@ -96,8 +94,8 @@ md"""
 
 # ╔═╡ d12e83b5-8351-44ef-aa4c-b5ace3b4eb39
 OBSERVATORIES = OrderedDict(
-	"FBO Colgate, Hamilton, NY: 0.4 m" => load(download("https://github.com/icweaver/UCAN/raw/refs/heads/main/asteroid_astrometry/asteroid_parallax/data/ASTEAST.FTS"))[:, :, 1],
-	"NURO, Flagstaff, AZ: 0.8 m" => load(download("https://github.com/icweaver/UCAN/raw/refs/heads/main/asteroid_astrometry/asteroid_parallax/data/ASTWEST.FTS"))[:, :, 1],
+	"eVscope East" => load("./data/nctq52_2022-02-25T19-58-34.991_Science_Defense_103.fits"),
+	"eVscope West" => load("./data/p4eaaw_2022-02-25T19-58-34.183_Science_Defense_125.fits"),
 );
 
 # ╔═╡ b4119602-990d-47b0-8ea5-7f14e17d9e9f
@@ -107,10 +105,10 @@ OBSERVATORIES = OrderedDict(
 """
 
 # ╔═╡ 0b7fcb43-ccb0-4708-9aed-9f8774ef8749
-img_1 = OBSERVATORIES["FBO Colgate, Hamilton, NY: 0.4 m"];
+img_1 = OBSERVATORIES["eVscope East"];
 
 # ╔═╡ 5523bfd6-4d1c-472f-a028-266b9a891df8
-img_2 = OBSERVATORIES["NURO, Flagstaff, AZ: 0.8 m"];
+img_2 = OBSERVATORIES["eVscope West"];
 
 # ╔═╡ f4d52a6a-644e-4ff1-861f-a0531c596040
 @mdx """
@@ -126,6 +124,12 @@ img_2 = OBSERVATORIES["NURO, Flagstaff, AZ: 0.8 m"];
 
 # ╔═╡ 10c77d40-dcb9-4620-b9c1-a47a56621a0c
 OBSERVATORIES[observatory]
+
+# ╔═╡ 738ef43b-0433-405a-9a63-9074edbfeb14
+@mdx """
+!!! tip " "
+	Associated header file information for $(observatory)
+"""
 
 # ╔═╡ 21afa6af-1df0-4c47-b106-1b9d1e161aa7
 header(OBSERVATORIES[observatory])
@@ -160,9 +164,9 @@ header(OBSERVATORIES[observatory])
 
 # ╔═╡ 6fc4ec56-0591-4f61-bdce-43ef796ab3a5
 point_map = (
-	[53, 220] => [81, 211], # Star 1
-	[268, 187] => [211, 191], # Star 2
-	[358, 48] => [266, 108], # Star 3
+	[1893, 1343] => [1219, 845],
+	[1779, 1177] => [1077, 709],
+	[1525, 1039] => [799, 625],
 );
 
 # ╔═╡ cf81d974-2bf7-4da0-a781-3519953554ff
@@ -175,7 +179,10 @@ tfm = kabsch(last.(point_map) => first.(point_map); scale=true);
 """
 
 # ╔═╡ 568347fb-92a3-4435-8204-80a1a0a1eaef
-PlutoUI.ExperimentalLayout.hbox([tfm.linear, tfm.translation])
+linear_matrix = tfm.linear
+
+# ╔═╡ 68faa552-2651-4349-9a54-8c9f82e86158
+translation_vector = tfm.translation
 
 # ╔═╡ a3a65c1c-a44e-475e-8044-35c453709483
 @mdx """
@@ -202,7 +209,7 @@ We are using [AstroImages.jl](https://juliaastro.org/dev/modules/AstroImages/) t
 """
 
 # ╔═╡ 28a823e7-66ee-4688-b0a3-1ebd776f129f
-@bind img_compare Select([img_1 => "img_1", img_2w => "img_2 (stacked)"])
+@bind img_compare Select([img_1 => "eVscope East", img_2w => "eVscope West (stacked)"])
 
 # ╔═╡ 983a1c03-0344-4905-8cf2-b799003eb94c
 img_compare
@@ -219,8 +226,11 @@ img_compare
 # ╔═╡ 13e464bb-30d2-4e6e-b038-69871acbba65
 @bind i Clock(max_value=2, repeat=true, start_running=true)
 
+# ╔═╡ 376cb78b-765a-4d8a-aaef-884ec579a2b5
+imgs = [img_1, img_2w];
+
 # ╔═╡ cd32b1d0-f455-4822-ad19-6560044d6c4a
-(img_1, img_2w)[i]
+imgs[i]
 
 # ╔═╡ 2f9fed6d-0fb0-4a1e-afad-6d7beda95ba1
 @mdx """
@@ -279,7 +289,7 @@ img_compare
 end
 
 # ╔═╡ 527c1ad3-02d1-4ea5-98b2-d0054f6b5a91
-plate_scale = 0.99 # ''/px
+plate_scale = 1.326 #0.99 # ''/px
 
 # ╔═╡ aabbcb9d-4310-437f-b7da-03b385916400
 θ = let
@@ -291,7 +301,7 @@ end
 # ╔═╡ 43a16d76-7de9-4f19-b1e4-a03457fd1e11
 @mdx """
 !!! note " "
-	Multiplying the Pythagorean distance between these two points by the known [plate scale](https://en.wikipedia.org/wiki/Plate_scale) of our reference image ($(plate_scale) "/pixel) then gives a parallax shift of approximately ``\\theta = $(round(θ; digits=2))''``.
+	Multiplying the Pythagorean distance between these two points by the known [plate scale](https://en.wikipedia.org/wiki/Plate_scale) of our reference image ``($(plate_scale)'' / \\mathrm{pixel})`` then gives a parallax shift of approximately ``\\theta = $(round(θ; digits=2))''``.
 """
 
 # ╔═╡ 864d23ed-d44e-4d3b-887c-73e49a909071
@@ -304,7 +314,7 @@ end
 
 # ╔═╡ 6bed5463-00a8-4c73-b0dc-6c7397c7a099
 # Baseline (kilometers)
-b = 3172
+b = 621 #3172
 
 # ╔═╡ 65d2286a-2786-4f96-8193-d0c4fe77d57a
 @mdx """
@@ -319,16 +329,10 @@ b = 3172
 d = 0.00138 * b / θ
 
 # ╔═╡ 202acfd6-1123-4e16-8d93-b9071006666c
-d0 = 0.26173027005421 # Actual distance
-
-# ╔═╡ 0043f0a6-d309-4527-a554-d37c73c36dfa
-accuracy = 100.0 * (d - d0) / d0 # Percent diff
-
-# ╔═╡ 2c5f91b7-4baf-4bbe-8634-c69223916e8f
-@mdx """
-!!! note " "
-	Based on our measurements, we estimate that the asteroid was about **$(round(d; digits=3)) AU** away from the Earth at the time of observation. This is within **$(abs(round(Int, accuracy)))%** of the true distance reported by [JPL](https://ssd.jpl.nasa.gov/horizons/app.html#/)!
-"""
+# ╠═╡ disabled = true
+#=╠═╡
+# d0 = 0.26173027005421 # Actual distance
+  ╠═╡ =#
 
 # ╔═╡ df00e717-2574-4281-9838-1f446960731a
 md"""
@@ -338,6 +342,9 @@ md"""
 
 # ╔═╡ 8479dc29-366f-43b2-bfed-2c2b38dd72f8
 d_units = (b * u"km") / (θ * u"arcsec") |> us"Constants.au"
+
+# ╔═╡ 95410159-1270-4e44-9ed3-ca41bf61a05c
+ustrip(d_units)
 
 # ╔═╡ dd382487-c181-4aad-b9c5-2e9bc422ed01
 md"""
@@ -363,11 +370,20 @@ md"""
 
 # ╔═╡ 53512b8f-3120-4f9a-bb5f-ff8aefc96408
 # Query 1 minute before and after the parallax observation
-df_ephem = ephemeris("9058", "1992-05-23T06:56", "1992-05-23T06:58", "1 min"; wrt="earth", units="AU-D") |> DataFrame
+df_ephem = ephemeris("153591", "2022-02-25T19:57", "2022-02-25T19:59", "1 minute"; wrt="earth", units="AU-D") |> DataFrame
 
 # ╔═╡ 7869c86f-52c3-4b3b-be59-b4f7626faf12
 # Row 2 has our desired timestamp
-df_ephem[2, [:x, :y, :z]] |> norm
+d0 = df_ephem[2, [:x, :y, :z]] |> norm
+
+# ╔═╡ 0043f0a6-d309-4527-a554-d37c73c36dfa
+accuracy = 100.0 * (d - d0) / d0 # Percent diff
+
+# ╔═╡ 2c5f91b7-4baf-4bbe-8634-c69223916e8f
+@mdx """
+!!! note " "
+	Based on our measurements, we estimate that the asteroid was about **$(round(d; digits=3)) AU** away from the Earth at the time of observation. This is within **$(abs(round(Int, accuracy)))%** of the true distance reported by [JPL](https://ssd.jpl.nasa.gov/horizons/app.html#/)!
+"""
 
 # ╔═╡ 9159cb78-6d0e-4c12-8f42-6b8e8316d167
 """
@@ -410,8 +426,8 @@ function trace_hm(img; colorbar_x=0)
 	imgv = permutedims(imgv)
 	zmin, zmax = zscale(imgv)
 	return heatmap(x=dims(imgv, X).val, y=dims(imgv, Y).val, z=imgv.data;
-		zmin,
-		zmax,
+		zmin = 2496.0,
+		zmax = 9029.4,
 		colorscale = :Cividis,
 		colorbar=attr(x=colorbar_x, thickness=10, title="Counts"),
 	)
@@ -450,7 +466,19 @@ end
 plot_pair(img_1, img_2)
 
 # ╔═╡ 84c11014-8890-4348-96b6-8e701e458de4
-plot_pair(img_1, img_2w.data)
+plot_pair(img_1, img_2w)
+
+# ╔═╡ fa433777-d76f-4c8a-b3f5-862ea5611328
+const ZMIN, ZMAX = let
+	lims = zscale.((img_1, img_2))
+	minimum(first, lims), maximum(last, lims)
+end
+
+# ╔═╡ 0495f2cf-9cf9-4402-85a2-67d537cfbdfb
+AstroImages.set_cmap!(colormap)
+
+# ╔═╡ b484f2c6-e8ea-4c0b-88ee-958cc53a9bff
+AstroImages.set_clims!((ZMIN, ZMAX))
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -1889,7 +1917,8 @@ version = "17.4.0+2"
 # ╟─f4d52a6a-644e-4ff1-861f-a0531c596040
 # ╟─59c58698-bb4f-4cc1-b8e7-721b1d70f5ef
 # ╟─10c77d40-dcb9-4620-b9c1-a47a56621a0c
-# ╟─21afa6af-1df0-4c47-b106-1b9d1e161aa7
+# ╟─738ef43b-0433-405a-9a63-9074edbfeb14
+# ╠═21afa6af-1df0-4c47-b106-1b9d1e161aa7
 # ╟─8bbccbde-4850-4f9d-941a-b163e2133afc
 # ╟─2bbcab7e-ee23-4136-b686-5472e61cd117
 # ╟─51186ae1-baac-4868-950f-1c9a86d720d8
@@ -1898,6 +1927,7 @@ version = "17.4.0+2"
 # ╠═cf81d974-2bf7-4da0-a781-3519953554ff
 # ╟─6193211b-8ec0-4f88-87df-35247c01353a
 # ╟─568347fb-92a3-4435-8204-80a1a0a1eaef
+# ╟─68faa552-2651-4349-9a54-8c9f82e86158
 # ╟─a3a65c1c-a44e-475e-8044-35c453709483
 # ╠═a9960706-4f5b-41e9-8dd4-2fbf24f4daec
 # ╟─4ed0072f-6159-4893-b21b-e035e51a6689
@@ -1906,10 +1936,9 @@ version = "17.4.0+2"
 # ╟─983a1c03-0344-4905-8cf2-b799003eb94c
 # ╟─961db3d8-9b31-4489-98d3-a66be4c9034c
 # ╟─13e464bb-30d2-4e6e-b038-69871acbba65
+# ╠═376cb78b-765a-4d8a-aaef-884ec579a2b5
 # ╟─cd32b1d0-f455-4822-ad19-6560044d6c4a
 # ╟─2f9fed6d-0fb0-4a1e-afad-6d7beda95ba1
-# ╟─fdd7bfe6-d4f7-434e-bac3-dc8994a17a6e
-# ╟─64cf11a7-09ef-459a-98b5-3e5f8a8cd1b5
 # ╟─8e0e738d-6bdf-4992-bc0e-ea00ea9617ba
 # ╟─3e475b77-638c-4bb2-81c6-d7146b72c41f
 # ╟─84c11014-8890-4348-96b6-8e701e458de4
@@ -1917,11 +1946,14 @@ version = "17.4.0+2"
 # ╟─43a16d76-7de9-4f19-b1e4-a03457fd1e11
 # ╠═527c1ad3-02d1-4ea5-98b2-d0054f6b5a91
 # ╠═aabbcb9d-4310-437f-b7da-03b385916400
+# ╠═fdd7bfe6-d4f7-434e-bac3-dc8994a17a6e
+# ╠═64cf11a7-09ef-459a-98b5-3e5f8a8cd1b5
 # ╟─864d23ed-d44e-4d3b-887c-73e49a909071
 # ╠═6bed5463-00a8-4c73-b0dc-6c7397c7a099
 # ╠═53e5fca6-41ee-4a46-9a41-d9f4e0673c8e
+# ╠═95410159-1270-4e44-9ed3-ca41bf61a05c
 # ╟─2c5f91b7-4baf-4bbe-8634-c69223916e8f
-# ╟─202acfd6-1123-4e16-8d93-b9071006666c
+# ╠═202acfd6-1123-4e16-8d93-b9071006666c
 # ╟─0043f0a6-d309-4527-a554-d37c73c36dfa
 # ╟─df00e717-2574-4281-9838-1f446960731a
 # ╠═50f77772-c419-4ad7-b9d6-12554302a518
@@ -1935,6 +1967,9 @@ version = "17.4.0+2"
 # ╟─e99ae23f-c998-4e09-8d24-5df55b4385ee
 # ╠═a23c40dc-0af3-4c3a-8172-203f58603bbb
 # ╠═05b2f9fe-61d2-4640-bbae-78d6d7465597
+# ╠═fa433777-d76f-4c8a-b3f5-862ea5611328
+# ╠═0495f2cf-9cf9-4402-85a2-67d537cfbdfb
+# ╠═b484f2c6-e8ea-4c0b-88ee-958cc53a9bff
 # ╠═db72ee5e-070b-4dff-b3b6-8b9915ed7b3e
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
